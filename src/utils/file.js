4bd4737faf44996b98affe312cf8ebe6fc015a34
MegeKaplan <kaplanmehmetege@gmail.com>
2025-08-09 00:30:38 +0300

    fix: use .webp extension for processed images instead of raw image extension

--- src/utils/file.js
+++ src/utils/file.js
@@ -2,5 +2,12 @@ import { extension } from "mime-types"
 
 export const generateObjectName = (mediaId, mimetype) => {
   const ext = extension(mimetype)
+  if (isImage(mimetype)) {
+    return `${mediaId}.webp`
+  }
   return `${mediaId}.${ext}`
+}
+
+export const isImage = (mimetype) => {
+  return mimetype.startsWith('image/')
 }
