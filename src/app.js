c69a79bd7d6043d840fd19857f4700fe01e67721
MegeKaplan <kaplanmehmetege@gmail.com>
2025-08-10 17:02:04 +0300

    feat: add cors middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import mediaRouter from './routes/media.route.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use("/media", mediaRouter)
 
